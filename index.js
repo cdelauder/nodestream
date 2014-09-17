94353c293d2104de84a30381fb767e53e3f732ea
cdelauder <cdelauder@gmail.com>
2014-09-17 16:18:28 -0700

    I did it! I have successfully used a stream for flow control. I am still not totally clear on what readable and writeable are because they dont seem to do the obvious stuff.

--- index.js
+++ index.js
@@ -3,29 +3,43 @@ var server = express()
 var express = require('express')
 var exphbs = require('express-handlebars')
 var querystring = require('querystring')
+var stream = require('stream')
 server.engine('handlebars', exphbs({defaultLayout: 'index'}));
 server.set('view engine', 'handlebars');
 
 
-server.get('/', function(req, res) {
+server.get('/', function (req, res) {
   res.render('form')
 })
 
-server.post('/info', function(req, res) {
+server.post('/info', function (req, res) {
   var info = 'a'
+  // var ws = new stream.Writeable()
+  // ws.on('data', function (data) {
+  //   console.log(data)
+  //   info = data
+  // })
+  // ws.on('end', function () {
+  //   res.render('form', {'info': info})
+  // })
+  var rs = new stream.Readable({ objectMode: true });
   req.on('data', function (chunk) {
     var body = chunk.toString()
     var form = querystring.parse(body)
-    info = changeFormData(form)
+    rs.push(changeFormData(form))
+    rs.push(null)
   })
-  res.render('form', {'info': info})
+  rs.on('data', function (data) {
+    res.render('form', {'info': data})
+  })
 })
 
 server.listen(3000)
 
-function changeFormData(data) {
+function changeFormData (data) {
   console.log('in ' + data['string'])
   var b = data['string'].split('').reverse().join("")
   console.log('out ' +b)
   return b
 }
+
