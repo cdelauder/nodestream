0502d5f3452f4f50bd265f3e89c99cdc33ced67d
cdelauder <cdelauder@gmail.com>
2014-09-17 15:28:09 -0700

    have a basic framework up. now will work on implementing streams while I run a time-consuming function

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var express = require('express')
 var server = express()
+var express = require('express')
+var exphbs = require('express-handlebars')
+var querystring = require('querystring')
+server.engine('handlebars', exphbs({defaultLayout: 'index'}));
+server.set('view engine', 'handlebars');
+
 
 server.get('/', function(req, res) {
-  res.set('Content-Type', 'text/html')
-  res.write('hello world')
-  res.send()
+  res.render('form')
+})
+
+server.post('/info', function(req, res) {
+  req.on('data', function (chunk) {
+    var body = chunk.toString()
+    var form = querystring.parse(body)
+    console.log(form)
+  })
+  res.render('form')
 })
 
 server.listen(3000)
+
