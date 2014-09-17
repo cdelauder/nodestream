4699b741632d41b7e0737d1a92a3c5ebe7a889b0
cdelauder <cdelauder@gmail.com>
2014-09-17 15:52:53 -0700

    have code that takes too long to execute to show up properly in the view. now i need to fix that

--- index.js
+++ index.js
@@ -12,13 +12,20 @@ server.get('/', function(req, res) {
 })
 
 server.post('/info', function(req, res) {
+  var info = 'a'
   req.on('data', function (chunk) {
     var body = chunk.toString()
     var form = querystring.parse(body)
-    console.log(form)
+    info = changeFormData(form)
   })
-  res.render('form')
+  res.render('form', {'info': info})
 })
 
 server.listen(3000)
 
+function changeFormData(data) {
+  console.log('in ' + data['string'])
+  var b = data['string'].split('').reverse().join("")
+  console.log('out ' +b)
+  return b
+}
