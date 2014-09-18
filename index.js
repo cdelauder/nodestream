7ff2fc374df918d6f7249a80527b068b5fe1c1dd
cdelauder <cdelauder@gmail.com>
2014-09-18 11:01:30 -0700

    have implemented a stream to handle an http request. used the through module, and finally get why it is a good thing. currently unable to render the html because i am using handlebars templates.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express')
 var exphbs = require('express-handlebars')
 var querystring = require('querystring')
 var stream = require('stream')
+var through = require('through')
 server.engine('handlebars', exphbs({defaultLayout: 'index'}));
 server.set('view engine', 'handlebars');
 
@@ -13,8 +14,7 @@ server.get('/', function (req, res) {
 })
 
 server.post('/info', function (req, res) {
-  var info = 'a'
-  var rs = new stream.Readable({ objectMode: true });
+  var rs = new stream.Readable();
   req.on('data', function (chunk) {
     var body = chunk.toString()
     var form = querystring.parse(body)
@@ -26,11 +26,39 @@ server.post('/info', function (req, res) {
   })
 })
 
+server.get('/pic', function (req, res) {
+  callAPI()
+  var html = ''
+  var through = require('through')
+
+  function write (buf) {
+    html += buf.toString()
+  }
+  
+  function end () {
+    res.render('form', {'pic':html})
+  }
+
+  function callAPI () {
+    var http = require('https')
+    var body = ''
+    http.get('https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=cute%20puppy%20pictures', function (res) {
+      res.on('data', function (chunk) {
+        body += chunk.toString()
+      })
+      res.on('end', function () {
+        return body
+      }).pipe(through(write, end))
+    })
+  }
+})
+
 server.listen(3000)
 
 function changeFormData (data) {
   var b = data['string'].split('').reverse().join("")
   return b
 }
+
 
 
